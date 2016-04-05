eb05acf19314975f09a3126c4e3c42ec28dc875e
Jose Escobar <jje001@email.uark.edu>
2016-04-05 13:14:20 -0500

    little more xml
    xml

--- Scurry/app/src/main/java/edu/uark/jfrorieemail/scurry/LoginScreen.java
+++ Scurry/app/src/main/java/edu/uark/jfrorieemail/scurry/LoginScreen.java
@@ -1,5 +1,7 @@
 package edu.uark.jfrorieemail.scurry;
 
+
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,8 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        ActionBar m_myActionBar=getSupportActionBar();
+        m_myActionBar.hide();
+
     }
 }
