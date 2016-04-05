346cc766aa80f6c6725ed90e8d232451b6b8270e
Jose Escobar <jje001@email.uark.edu>
2016-04-05 14:58:21 -0500

    LoginScreen java
    *checks if email is valid
    *transitions to the next page

--- Scurry/app/src/main/java/edu/uark/jfrorieemail/scurry/LoginScreen.java
+++ Scurry/app/src/main/java/edu/uark/jfrorieemail/scurry/LoginScreen.java
@@ -1,9 +1,11 @@
 package edu.uark.jfrorieemail.scurry;
 
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -11,8 +13,39 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-        ActionBar m_myActionBar=getSupportActionBar();
-        m_myActionBar.hide();
 
+    }
+
+    public void onLogin(View view){
+
+        EditText email_et = (EditText) findViewById(R.id.email);
+        EditText password_et = (EditText) findViewById(R.id.password);
+
+        String email_val = email_et.getText().toString();
+        String password_val = password_et.getText().toString();
+
+        if(isEmailValid(email_val) == false){
+            Toast.makeText(getApplicationContext(), "Email not valid", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            //check DB for email
+            //pull password for that email
+            //compare both
+            //if same -> setContentView(R.layout.main_page);
+            //else -> Toast "Incorrect email and password combination. Try again"
+
+            //transition purposes
+            //if NOT null
+            if(password_val.matches("")){
+                Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                setContentView(R.layout.main_page);
+            }
+        }
+    }
+
+    private boolean isEmailValid(String email){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
